61adf874eced0def023349b5cfad7dc38ba7e0bb
ActuallyAJL <zero3260@hotmail.com>
2022-08-17 11:52:17 -0500

    8kyu

---
+++ sql/8kyu/evenOrOdd.sql
@@ -0,0 +1,20 @@
+--__Decription__
+
+--You will be given a table, numbers, with one column number.
+
+--Return a table with a column is_even containing "Even" or "Odd" depending on number column values.
+
+--numbers table schema
+--number INT
+--output table schema
+--is_even STRING
+
+--__Solution__
+
+SELECT 
+  CASE
+    WHEN number % 2 = 0 THEN 'Even'
+    ELSE 'Odd'
+  END
+AS is_even
+FROM numbers;
---
+++ sql/8kyu/sqlBasicsMod.sql
@@ -0,0 +1,14 @@
+--__Description__
+
+--Given the following table 'decimals':
+
+--** decimals table schema **
+
+--id
+--number1
+--number2
+--Return a table with one column (mod) which is the output of number1 modulus number2.
+
+--__Solutions__
+
+SELECT (number1 % number2) AS mod FROM decimals
