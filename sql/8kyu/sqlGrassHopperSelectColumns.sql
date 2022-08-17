60e633d12e1e196faf3771882d89f6a8f314dc88
ActuallyAJL <zero3260@hotmail.com>
2022-08-17 10:56:19 -0500

    8kyu

---
+++ sql/8kyu/sqlGrassHopperSelectColumns.sql
@@ -0,0 +1,7 @@
+--__Description__
+
+--Using only SQL, write a query that returns all rows in the custid, custname, and custstate columns from the customers table.
+
+--__Solution__
+
+SELECT custid, custname, custstate FROM customers;
