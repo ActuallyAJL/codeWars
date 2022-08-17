22151fe59087272c5da6a7995b64243b75a6123c
ActuallyAJL <zero3260@hotmail.com>
2022-08-17 11:00:16 -0500

    8kyu

---
+++ sql/8kyu/easySqlLowerCase.sql
@@ -0,0 +1,15 @@
+--__Description__
+
+--Given a demographics table in the following format:
+
+--** demographics table schema **
+
+--id
+--name
+--birthday
+--race
+--you need to return the same table where all letters are lowercase in the race column.
+
+--__Solution__
+
+SELECT id, name, birthday, LOWER(race) AS race FROM demographics
